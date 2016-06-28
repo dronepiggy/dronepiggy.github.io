0b612ea126f3920f78433a820b30eb07cac9e02e
Zak Crawford <zak.crawford@chartboost.com>
2016-06-28 13:19:24 -0700

    absolute refs only

--- _plugins/hex_to_rgb.rb
+++ _plugins/hex_to_rgb.rb
@@ -21,7 +21,7 @@ module Jekyll
   module HexToRGB
     def hex_to_rgb(hexval)
       if hexval.length.even?
-        hexval.scan(/../).map {|color| color.to_i(16)}
+        hexval.scan(//).map {|color| color.to_i(16)}
       else
         hexval.scan(/./).map {|color| (color+color).to_i(16)}
       end
